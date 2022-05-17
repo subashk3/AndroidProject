f51746ca22f7253a2f7d917598d94d24c6d939e3
Subash K <98878397+subashk3@users.noreply.github.com>
2022-05-17 18:57:59 +0530

    Last Commit

--- ViewModel/app/src/main/java/com/example/viewmodel/viewmodel/MyViewModel.kt
+++ ViewModel/app/src/main/java/com/example/viewmodel/viewmodel/MyViewModel.kt
@@ -2,10 +2,13 @@ package com.example.viewmodel.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
     var counter = 0
     val currentCounter: MutableLiveData<Int> by lazy {
         MutableLiveData<Int>()
     }
+
 }
