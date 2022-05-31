ae3c2af2c71ee09b11ed308e7194cb400f240c31
saravanan-selvam <43129002+saravanan-selvam@user.noreply.github.com>
2022-05-31 16:09:36 +0530

    Commit 31 May

--- Contacts/app/src/main/java/com/example/contacts/ContactList.kt
+++ Contacts/app/src/main/java/com/example/contacts/ContactList.kt
@@ -1,6 +1,7 @@
 package com.example.contacts
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -50,6 +51,7 @@ class ContactList : Fragment() {
 
             } else {
                 recyclerView.adapter = contactAdapter
+                Log.d("","")
             }
         }
 
